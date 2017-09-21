b60b9645c7ba8fe7c8892b3e3a30c22595f2ecc8
Carsten Witt <carsten.witt@gmail.com>
2017-09-21 15:18:43 +0200

    Test on PSR-11 exceptions

--- tests/src/CategoriesTest.php
+++ tests/src/CategoriesTest.php
@@ -2,8 +2,11 @@
 namespace tests;
 
 use Germania\Categories\Categories;
+use Germania\Categories\CategoryInterface;
+use Germania\Categories\CategoryNotFoundException;
 use Interop\Container\ContainerInterface;
 use Psr\Container\ContainerInterface as PsrContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class CategoriesTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,6 +24,41 @@ class CategoriesTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(PsrContainerInterface::class, $sut);
         $this->assertInstanceOf(\Iterator::class, $sut->getIterator());
 
+    }
+
+
+    public function testPushMethod()
+    {
+        $cat_id = 1;
+
+        $cat_mock = $this->prophesize(CategoryInterface::class );
+        $cat_mock->getId()->willReturn( $cat_id );
+        $cat = $cat_mock->reveal();
+
+        $sut = new Categories;
+        $original_count = count($sut);
+        $this->assertFalse($sut->has( $cat_id ));
+        $this->assertSame($sut, $sut->push( $cat ));
+        $this->assertTrue($sut->has( $cat_id ));
+        $this->assertSame( $sut->get( $cat_id), $cat);
+        $this->assertGreaterThan($original_count, count($sut));
+    }
+
+    public function testCategoryNotFoundException()
+    {
+        $cat_id = "does_not_exist";
+
+        $cat_mock = $this->prophesize(CategoryInterface::class );
+        $cat_mock->getId()->willReturn( $cat_id );
+        $cat = $cat_mock->reveal();
+
+        $sut = new Categories;
+        $original_count = count($sut);
+
+        $this->assertFalse($sut->has( $cat_id ));
+        $this->expectException( CategoryNotFoundException::class );
+        $this->expectException( NotFoundExceptionInterface::class );
+        $sut->get( $cat_id);
     }
 
 
